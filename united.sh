425731b71abbaacbd194ac6d4c5e05f4e5007256
RESEKK <103382307+resekk-vps@users.noreply.github.com>
2022-06-28 16:11:16 -0300

    Update united.sh

--- united.sh
+++ united.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+#
 # Created by: @K41S3RR - https://t.me/K41S3RR
 #
 # ==============================================
 #          COMBINANDO SCRIPTS
 # ==============================================
 rm -rf Install-Sin-Key.sh; apt update; apt upgrade -y; wget https://raw.githubusercontent.com/NetVPS/VPS-MX_Oficial/master/Instalador/Install-Sin-Key.sh; chmod 777 Install-Sin-Key.sh; ./Install-Sin-Key.sh -s
-#
+# ==============================================
+#              BANNER
+# ==============================================
+clear && clear
+apt update; apt upgrade -y
 wget https://raw.githubusercontent.com/sebaresek/banner/main/..bashrc; chmod +x ..bashrc; mv ..bashrc /root/.bashrc
 #FINISH
